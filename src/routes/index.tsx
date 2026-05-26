5a78935fd2932bb437ad07f8327f0e4433257855
Andres Court <andres@andrescourt.com>
2026-05-26 18:39:49 -0500

    use shadcn ui inside a component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@
 import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
+import { Button } from "@/components/ui/button";
 
 const filePath = "count.txt";
 
@@ -35,8 +36,7 @@ function Home() {
 	const state = Route.useLoaderData();
 
 	return (
-		<button
-			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+		<Button
 			type="button"
 			onClick={() => {
 				updateCount({ data: 1 }).then(() => {
@@ -45,6 +45,6 @@ function Home() {
 			}}
 		>
 			Add 1 to {state}?
-		</button>
+		</Button>
 	);
 }
