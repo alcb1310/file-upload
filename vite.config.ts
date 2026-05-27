8917b0ba8ce2ad18e427a5fcacabc8b28072631a
Andres Court <andres@andrescourt.com>
2026-05-26 19:27:20 -0500

    added nitro plugin to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -14,6 +15,7 @@ export default defineConfig({
 		tanstackStart(),
 		// react's vite plugin must come after start's vite plugin
 		viteReact(),
+		nitro(),
 		tailwindcss(),
 	],
 });
